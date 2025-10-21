0b45268fabe3560fb18f38d0b9b057520e2bfb0d
HajimeKouno <kura.ai.3698@gmail.com>
2025-10-22 06:15:58 +0900

    下準備

--- home.php
+++ home.php
@@ -12,8 +12,19 @@
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
-    <main>
-
+    <main class="l-salon-charm">
+        <section class="p-fv"></section>
+        <section class="p-problem"></section>
+        <section class="p-insight"></section>
+        <section class="p-feature"></section>
+        <section class="p-benefit"></section>
+        <section class="p-easeofuse"></section>
+        <section class="p-overview"></section>
+        <section class="p-plan"></section>
+        <section class="p-voice"></section>
+        <section class="p-credibility"></section>
+        <section class="p-faq"></section>
+        <section class="p-action"></section>
     </main>
 
     <?php get_footer(); ?>
